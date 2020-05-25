834ccbea203f4b177f2e7af8ce30638af9495978
Eduardo B Colombo <eduardobcolombo@gmail.com>
2020-05-25 11:29:27 -0300

    added tobias endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,7 +9,14 @@ app.get('/brazil', function (req, res) {
   res.send('Hello Brazil!\n');
 });
 
+app.get('/tobiascolombo', function(req, res) {
+  res.send('Eu sou o Tobias Colombo');
+});
+
+
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
 
